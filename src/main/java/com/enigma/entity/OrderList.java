2f2c66a5bd76f8e465a880bba96b9a4c7781ca46
drikalabs <andrika.bogal@gmail.com>
2019-11-06 20:00:51 +0700

    create orderList entity

---
+++ src/main/java/com/enigma/entity/OrderList.java
@@ -0,0 +1,92 @@
+package com.enigma.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class OrderList {
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid",strategy = "uuid")
+    private String idOrder;
+
+    @ManyToOne
+    @JoinColumn(name = "id_table")
+    private TableEntities table;
+    private String picCustomer;
+    private BigDecimal totalPrice;
+    private Integer manyCustomers;
+    @OneToMany(mappedBy = "idOrder",cascade = CascadeType.PERSIST)
+    private List<OrderDetail>orderDetails=new ArrayList<>();
+
+    @Transient
+    private  String idTable;
+
+    public OrderList(String picCustomer, BigDecimal totalPrice, Integer manyCustomers, String idTable) {
+        this.picCustomer = picCustomer;
+        this.totalPrice = totalPrice;
+        this.manyCustomers = manyCustomers;
+        this.idTable = idTable;
+    }
+
+    public OrderList() {
+    }
+
+    public String getIdOrder() {
+        return idOrder;
+    }
+
+    public void setIdOrder(String idOrder) {
+        this.idOrder = idOrder;
+    }
+
+    public TableEntities getTable() {
+        return table;
+    }
+
+    public void setTable(TableEntities table) {
+        this.table = table;
+    }
+
+    public String getPicCustomer() {
+        return picCustomer;
+    }
+
+    public void setPicCustomer(String picCustomer) {
+        this.picCustomer = picCustomer;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Integer getManyCustomers() {
+        return manyCustomers;
+    }
+
+    public void setManyCustomers(Integer manyCustomers) {
+        this.manyCustomers = manyCustomers;
+    }
+
+    public List<OrderDetail> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(List<OrderDetail> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
+
+    public String getIdTable() {
+        return idTable;
+    }
+
+    public void setIdTable(String idTable) {
+        this.idTable = idTable;
+    }
+}
