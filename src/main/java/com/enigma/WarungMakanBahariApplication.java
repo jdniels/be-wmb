a066f9b6aa2cfff1cc713b3d4bc00386d6ae4d3b
Juan Daniel <jdniels525@gmail.com>
2019-11-06 16:38:16 +0700

    initial commit

---
+++ src/main/java/com/enigma/WarungMakanBahariApplication.java
@@ -0,0 +1,13 @@
+package com.enigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarungMakanBahariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarungMakanBahariApplication.class, args);
+    }
+
+}
