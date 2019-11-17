a98dd549ccf264ed70a4acd87b8529db2ccb1906
drikalabs <andrika.bogal@gmail.com>
2019-11-17 18:52:53 +0700

    testing transaction service

--- src/test/java/com/enigma/service/serviceImpl/ImplementTransactionTest.java
+++ src/test/java/com/enigma/service/serviceImpl/ImplementTransactionTest.java
@@ -110,6 +110,26 @@ public class ImplementTransactionTest {
 
     @Test
     public void updatePaymentStatus() {
+        TableEntities newTable = new TableEntities(3, "AVAILABLE", 5);
+        newTable = tableRepositories.save(newTable);
+        FoodEntities newFood1 = new FoodEntities("pecel", "MAKANAN", 10000, 50);
+        FoodEntities newFood2 = new FoodEntities("sup", "MAKANAN", 10000, 50);
+        newFood1 = foodRepositories.save(newFood1);
+        newFood2 = foodRepositories.save(newFood2);
+        List<OrderDetail> orderDetails = new ArrayList<>();
+        orderDetails.add(new OrderDetail(2, newFood1.getIdFood()));
+        orderDetails.add(new OrderDetail(2, newFood2.getIdFood()));
+        OrderList newOrderList = new OrderList("RIfqi", 4, orderDetails, newTable.getIdTable());
+        newOrderList = orderListService.saveOrder(newOrderList);
+        Transaction transaction= transactionService.saveTransaction(newOrderList);
+        Transaction transaction1= new Transaction();
+        transaction1.setIdTransaction(transaction.getIdTransaction());
+        transaction1.setPay(100000);
+        transaction1.setOrderList(transaction.getOrderList());
+        transaction1.setPaymentDate(transaction.getPaymentDate());
+        transaction1.setPaymentStatus(transaction.getPaymentStatus());
+        transaction1.setTotal(transaction.getTotal());
+        assertEquals(transactionService.updatePaymentStatus(transaction1),transactionRepositories.findById(transaction.getIdTransaction()).get());
     }
 
     @Test
